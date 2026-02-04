c32082ad85ed687a5574dc785a309c9af8546cdf
alisgh <alisg@live.fr>
2026-02-04 23:42:00 +0100

    Your commit message

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import PLCConfigPage from "./pages/PLCConfig";
 import NotFound from "./pages/NotFound";
+import NutrientSchedule from "./components/NutrientSchedule";
 
 const queryClient = new QueryClient();
 
