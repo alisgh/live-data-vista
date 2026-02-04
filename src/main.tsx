07190faf8b33a79693f939356c24b2db37edc680
alisgh <alisg@live.fr>
2026-02-04 23:58:41 +0100

    Your commit message

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import Watering2 from './components/Watering2';
 
 createRoot(document.getElementById("root")!).render(
-  <>
+  <React.StrictMode>
     <App />
     <Watering2 />
-  </>
+  </React.StrictMode>
 );
